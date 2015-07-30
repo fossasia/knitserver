ecc6490a168a789c591fee0eea3ef9f5b8989a2d
Sebastian Oliva <code@sebastianoliva.com>
2015-07-29 20:44:12 -0600

    Adding config module.

---
+++ config_module.py
@@ -0,0 +1,16 @@
+__author__ = 'tian'
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = 'sqlite://:memory:'
+
+class ProductionConfig(Config):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
