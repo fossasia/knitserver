6d76e8c8cee25b3a7a710b7e3c2e8a7931640e9c
Sebastian Oliva <code@sebastianoliva.com>
2015-08-13 00:12:27 -0600

    Added create_knitjob, init and config operation functions in JS template.

--- knitlib_webserver.py
+++ knitlib_webserver.py
@@ -81,14 +81,88 @@ def socket_test_page():
             };
 
             ws.onopen = function(){
-                ws.send("hello john");
+                ws.send("hello");
+            };
+
+            var create_knitjob = function () {
+              knitjob_id = ""
+              $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: "//"+location.host + "/v1/create_job/",
+                data: {
+                  "plugin_id": "dummy",
+                  "port": "/dev/null"
+                },
+                success: function(data){
+                  console.log("Created knitting job:")
+                  console.log(data);
+                  knitjob_id = data["job_id"];
+                }
+              });
+              return knitjob_id;
+            };
+
+            var init_knitjob = function (job_id) {
+              $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: "//"+location.host + "/v1/init_job/" + job_id,
+                data: {
+                  /* No data should be needed for job init. */
+                },
+                success: function(data){
+                  console.log("Inited knitting job:")
+                  console.log(data);
+                }
+              });
+            };
+
+            var config_knitjob = function (job_id) {
+              /**
+              var fd = new FormData();
+              fd.append("knitpat_dict", {"colors": 2, "file_url":"embedded" });
+              */
+              $.ajax({
+                type: "POST",
+                dataType: "json",
+                // contentType: false,
+                contentType: "multipart/form-data",
+                url: "//"+location.host + "/v1/configure_job/" + job_id,
+                data: {
+                  "knitpat_dict": {"colors": 2, "file_url":"embedded" }
+                  // TODO: add image data
+                  // "file": new Blob()
+                },
+                success: function(data){
+                  console.log("Configured knitting job:")
+                  console.log(data);
+                }
+              });
+            };
+
+            var knit_knitjob = function (job_id) {
+              $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: "//"+location.host + "/v1/knit_job/" + job_id,
+                data: {
+                  /* No data should be needed */
+                },
+                success: function(data){
+                  console.log("Knitting knitting job:")
+                  console.log(data);
+                }
+              });
             };
         </script>
 
     </head>
 
     <body>
-        <p>hello world</p>
+        <p>Check out the console to test operation.</p>
+
+        <script src="//code.jquery.com/jquery-1.11.3.js"></script>
     </body>
 
 </html>
