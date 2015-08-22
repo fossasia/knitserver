408880668625e44ba19682993b069af68e046529
Sebastian Oliva <code@sebastianoliva.com>
2015-08-21 23:43:47 -0600

    Making functions generic.

--- static/knitlib_js_client.js
+++ static/knitlib_js_client.js
@@ -11,6 +11,13 @@ ws.onopen = function(){
     ws.send("hello");
 };
 
+knitpat_dict_demo_string = JSON.stringify({
+  "colors": 2,
+  "file_url": "embedded",
+  // TODO: add generated image data
+  "image_data": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP4z8CwCgAEqwGqN0e5wwAAAABJRU5ErkJggg=="
+});
+
 var create_knitjob = function (plugin_id, port, success_function) {
   $.ajax({
     type: "POST",
@@ -36,7 +43,7 @@ var init_knitjob = function (job_id, success_function) {
   });
 };
 
-var config_knitjob = function (job_id, success_function) {
+var config_knitjob = function (job_id, success_function, knitpat_dict_string) {
   
   //var fd = new FormData();
   // fd.append("knitpat_dict", JSON.stringify({"colors": 2, "file_url":"embedded" }));
@@ -46,13 +53,7 @@ var config_knitjob = function (job_id, success_function) {
     type: "POST",
     dataType: "json",
     url: "//"+location.host + "/v1/configure_job/" + job_id,
-    data: { "knitpat_dict":
-    JSON.stringify({
-      "colors": 2, 
-      "file_url":"embedded", 
-      // TODO: add generated image data
-      "image_data": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP4z8CwCgAEqwGqN0e5wwAAAABJRU5ErkJggg=="
-     })},
+    data: {"knitpat_dict": knitpat_dict_string},
     success: success_function
   });
 };
